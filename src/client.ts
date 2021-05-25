70b8bfe0bc0ddcf59bca8529c8368f07959ce5ff
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-05-25 22:40:12 +0200

    feat: added a logger to the global instances

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,5 @@
-import { Bot } from './Bot';
+import { Bot } from './classes/Bot/Bot';
+import { Logger } from './classes/Logger/Logger';
 
 export const bot = new Bot();
+export const logger = new Logger();
