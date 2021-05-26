94ede2cd96da9651dec866a81aa732214492398a
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-05-26 12:52:07 +0200

    feat: added a commandInteraction utility file

---
+++ src/utils/commandInteraction.ts
@@ -0,0 +1,18 @@
+import { CommandInteraction } from 'discord.js';
+
+/**
+ * Allows to easily retrieve useful informations from a `CommandInteraction` object.
+ * @param command The command you want to extract informations from
+ * @returns The extracted informations as an object
+ */
+export const extractCommandInfos = (command: CommandInteraction) => {
+   const guildObject = command.client.guilds.cache.get(command.guildID);
+   const channelObject = guildObject.channels.resolve(command.channelID);
+
+   const guild = guildObject.name;
+   const channel = channelObject.name;
+   const user = command.member.user.username;
+   const userId = `${command.member.user.id}`;
+
+   return { guild, channel, user, userId };
+};
