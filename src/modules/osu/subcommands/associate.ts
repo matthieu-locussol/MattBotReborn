bc33e3c3b1ac28d192a041c2fe7d44c397641004
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-05-27 09:13:40 +0200

    feat: updated associate osu subcommand to use cache

--- src/modules/osu/subcommands/associate.ts
+++ src/modules/osu/subcommands/associate.ts
@@ -1,5 +1,17 @@
 import { CommandInteraction } from 'discord.js';
+import { osuModule } from '..';
+import { extractCommandInfos } from '../../../utils/commandInteraction';
 
-export const associate = (command: CommandInteraction, username: string) => {
-   console.log('associate: ', username);
+export const associate = async (command: CommandInteraction, username: string) => {
+   const { userId, guildId } = extractCommandInfos(command);
+
+   const cache = await osuModule.cache.get(guildId);
+   const entry = {
+      userId,
+      username,
+   };
+
+   cache.associations.push(entry);
+
+   osuModule.cache.set(guildId, cache);
 };
