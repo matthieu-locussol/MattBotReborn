c4f48658215b2ca7cabbb7c6fde9c9e1d40b9803
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-06-02 17:30:05 +0200

    feat: added LogData elements

--- src/classes/Logger/LogData.ts
+++ src/classes/Logger/LogData.ts
@@ -1,5 +1,6 @@
 import { CommandInteractionOption } from 'discord.js';
 
+export const LOG_RAW_LOG = 'LOG_Raw_Log';
 export const LOG_LOGGED_IN = 'LOG_Logged_In';
 export const LOG_PERMISSIONLISTS_COLLISION = 'LOG_PermissionLists_Collision';
 export const LOG_ADD_MODULE = 'LOG_Add_Module';
@@ -19,8 +20,35 @@ export const LOG_OSU_ASSOCIATED_USERNAME = 'LOG_Osu_Associated_Username';
 export const LOG_LANG_UPDATED = 'LOG_Lang_Updated';
 export const LOG_BOT_JOINED_GUILD = 'LOG_Bot_Joined_Guild';
 export const LOG_BOT_REMOVED_GUILD = 'LOG_Bot_Removed_Guild';
+export const LOG_OSU_RECENT_GET_USERNAME_FROM_CACHE = 'LOG_Osu_Recent_Get_Username_From_Cache';
+export const LOG_OSU_RECENT_NO_USERNAME_MATCHING = 'LOG_Osu_Recent_No_Username_Matching';
+export const LOG_OSU_RECENT_RETRIEVING_SCORE = 'LOG_Osu_Recent_Retrieving_Score';
+export const LOG_OSU_RETRIEVING_BEATMAP = 'LOG_Osu_Retrieving_Beatmap';
+export const LOG_OSU_BEATMAP_FOUND_CACHE = 'LOG_Osu_Beatmap_Found_Cache';
+export const LOG_OSU_BEATMAP_NOT_FOUND_CACHE = 'LOG_Osu_Beatmap_Not_Found_Cache';
+export const LOG_OSU_BEATMAP_CACHED = 'LOG_Osu_Beatmap_Cached';
+export const LOG_OSU_RETRIEVING_USER = 'LOG_Osu_Retrieving_User';
+export const LOG_OSU_RECENT_CLAMPED = 'LOG_Osu_Recent_Clamped';
+export const LOG_OSU_RECENT_NOT_FOUND = 'LOG_Osu_Recent_Not_Found';
+export const LOG_OSU_RETRIEVING_BEST_SCORES = 'LOG_Osu_Retrieving_Best_Scores';
+export const LOG_OSU_BEST_SCORES_FOUND = 'LOG_Osu_Best_Scores_Found';
+export const LOG_OSU_RETRIEVING_SCORE_PP = 'LOG_Osu_Retrieving_Score_Pp';
+export const LOG_OSU_PP_COMPUTED = 'LOG_Osu_Pp_Computed';
+export const LOG_OSU_RETRIEVING_SCORES = 'LOG_Osu_Retrieving_Scores';
+export const LOG_OSU_SCORES_FOUND = 'LOG_Osu_Scores_Found';
+export const LOG_OSU_RETRIEVING_BEATMAP_FILE = 'LOG_Osu_Retrieving_Beatmap_File';
+export const LOG_OSU_BEATMAP_FILE_COMPUTED = 'LOG_Osu_Beatmap_File_Computed';
+export const LOG_OSU_BEATMAP_FILE_COMPUTED_FC = 'LOG_Osu_Beatmap_File_Computed_FC';
+export const LOG_OSU_BEATMAP_CHART_FOUND = 'LOG_Osu_Beatmap_Chart_Found';
+export const LOG_OSU_BEATMAP_CHART_NOT_FOUND = 'LOG_Osu_Beatmap_Chart_Not_Found';
+export const LOG_OSU_BEATMAP_CHART_RENDERING = 'LOG_Osu_Beatmap_Chart_Rendering';
+export const LOG_OSU_BEATMAP_CHART_CACHED = 'LOG_Osu_Beatmap_Chart_Cached';
 
 export type LogData =
+   | {
+        id: typeof LOG_RAW_LOG;
+        data: string;
+     }
    | {
         id: typeof LOG_LOGGED_IN;
         tag: string;
@@ -117,4 +145,101 @@ export type LogData =
         id: typeof LOG_BOT_REMOVED_GUILD;
         guild: string;
         guildId: string;
+     }
+   | {
+        id: typeof LOG_OSU_RECENT_GET_USERNAME_FROM_CACHE;
+     }
+   | {
+        id: typeof LOG_OSU_RECENT_NO_USERNAME_MATCHING;
+     }
+   | {
+        id: typeof LOG_OSU_RECENT_RETRIEVING_SCORE;
+        username: string;
+        index: number;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_BEATMAP;
+        beatmapId: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_FOUND_CACHE;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_NOT_FOUND_CACHE;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_CACHED;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_USER;
+        username: string;
+     }
+   | {
+        id: typeof LOG_OSU_RECENT_CLAMPED;
+        index: number;
+        limit: number;
+     }
+   | {
+        id: typeof LOG_OSU_RECENT_NOT_FOUND;
+        username: string;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_BEST_SCORES;
+        username: string;
+        limit: number;
+     }
+   | {
+        id: typeof LOG_OSU_BEST_SCORES_FOUND;
+        username: string;
+        count: number;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_SCORE_PP;
+        playerId: string;
+        beatmapId: string;
+     }
+   | {
+        id: typeof LOG_OSU_PP_COMPUTED;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_SCORES;
+        beatmapId: string;
+        limit: number;
+     }
+   | {
+        id: typeof LOG_OSU_SCORES_FOUND;
+        beatmapId: string;
+        count: number;
+     }
+   | {
+        id: typeof LOG_OSU_RETRIEVING_BEATMAP_FILE;
+        beatmapId: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_FILE_COMPUTED;
+        beatmapId: string;
+        acc: string;
+        pp: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_FILE_COMPUTED_FC;
+        beatmapId: string;
+        accFc: string;
+        ppFc: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_CHART_FOUND;
+        filename: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_CHART_NOT_FOUND;
+        filename: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_CHART_RENDERING;
+        filename: string;
+     }
+   | {
+        id: typeof LOG_OSU_BEATMAP_CHART_CACHED;
+        filename: string;
      };
--- src/classes/Logger/Logger.ts
+++ src/classes/Logger/Logger.ts
@@ -6,6 +6,7 @@ import { LogData } from './LogData';
 export class Logger {
    private _logs: LogData[];
    private _logger: winston.Logger;
+   private _loggerRaw: winston.Logger;
 
    constructor() {
       this._logs = [];
@@ -13,6 +14,17 @@ export class Logger {
          transports: [new winston.transports.Console()],
          format: winston.format.printf((log) => `[${log.level.toUpperCase()}] ${currentTime()} > ${log.message}`),
       });
+      this._loggerRaw = winston.createLogger({
+         transports: [new winston.transports.Console()],
+         format: winston.format.printf((log) => log.message),
+      });
+   }
+
+   logRaw(data: string) {
+      const log: LogData = { id: 'LOG_Raw_Log', data };
+
+      this._logs.push(log);
+      this._loggerRaw.log('info', data);
    }
 
    log(data: LogData) {
@@ -89,6 +101,75 @@ export class Logger {
          }
          case 'LOG_Bot_Removed_Guild': {
             return `MattBot has been removed from server "${data.guild}" (ID: ${data.guildId})`;
+         }
+         case 'LOG_Osu_Recent_Get_Username_From_Cache': {
+            return `--- No username provided, retrieving it from cache...`;
+         }
+         case 'LOG_Osu_Recent_No_Username_Matching': {
+            return `--- No username provided nor found in cache, aborting!`;
+         }
+         case 'LOG_Osu_Recent_Retrieving_Score': {
+            return `--- Retrieving the recent osu! score ${data.index} for username ${data.username}...`;
+         }
+         case 'LOG_Osu_Retrieving_Beatmap': {
+            return `--- Retrieving osu! beatmap ${data.beatmapId}...`;
+         }
+         case 'LOG_Osu_Beatmap_Found_Cache': {
+            return `--- Found beatmap in cache!`;
+         }
+         case 'LOG_Osu_Beatmap_Not_Found_Cache': {
+            return `--- Beatmap not found in cache, calling the API...`;
+         }
+         case 'LOG_Osu_Beatmap_Cached': {
+            return `--- Beatmap has been retrieved and cached!`;
+         }
+         case 'LOG_Osu_Retrieving_User': {
+            return `--- Retrieving the osu! user ${data.username}...`;
+         }
+         case 'LOG_Osu_Recent_Clamped': {
+            return `--- Index ${data.index} has been clamped to ${data.limit} because it was out of range`;
+         }
+         case 'LOG_Osu_Recent_Not_Found': {
+            return `--- No recent play found the osu! user ${data.username}!`;
+         }
+         case 'LOG_Osu_Retrieving_Best_Scores': {
+            return `--- Retrieving the osu! ${data.limit} best scores for username ${data.username}...`;
+         }
+         case 'LOG_Osu_Best_Scores_Found': {
+            return `--- Found ${data.count} osu! best scores for username ${data.username}!`;
+         }
+         case 'LOG_Osu_Retrieving_Score_Pp': {
+            return `--- Retrieving the osu! PP for a score set by the user ${data.playerId} on the beatmap ${data.beatmapId}...`;
+         }
+         case 'LOG_Osu_Pp_Computed': {
+            return `--- PP for the score have been computed!`;
+         }
+         case 'LOG_Osu_Retrieving_Scores': {
+            return `--- Retrieving the osu! ${data.limit} best scores on the beatmap ${data.beatmapId}...`;
+         }
+         case 'LOG_Osu_Scores_Found': {
+            return `--- Found ${data.count} osu! best scores on the beatmap ${data.beatmapId}!`;
+         }
+         case 'LOG_Osu_Retrieving_Beatmap_File': {
+            return `--- Downloading beatmap file for beatmap ${data.beatmapId} using curl...`;
+         }
+         case 'LOG_Osu_Beatmap_File_Computed': {
+            return `--- Computed score for beatmap ${data.beatmapId}: ${data.acc} accuracy and ${data.pp}pp`;
+         }
+         case 'LOG_Osu_Beatmap_File_Computed_FC': {
+            return `--- Computed FC score for beatmap ${data.beatmapId}: ${data.accFc} accuracy and ${data.ppFc}pp`;
+         }
+         case 'LOG_Osu_Beatmap_Chart_Found': {
+            return `--- Found chart ${data.filename} in cache!`;
+         }
+         case 'LOG_Osu_Beatmap_Chart_Not_Found': {
+            return `--- Could not find the chart ${data.filename}, generating it...`;
+         }
+         case 'LOG_Osu_Beatmap_Chart_Rendering': {
+            return `--- Rendering chart ${data.filename} on a canvas...`;
+         }
+         case 'LOG_Osu_Beatmap_Chart_Cached': {
+            return `--- Chart ${data.filename} has been generated and cached!`;
          }
       }
    }
