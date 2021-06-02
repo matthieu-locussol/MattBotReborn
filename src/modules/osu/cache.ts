21e28cc4dd8a38acf5d68e2fff41f441ca477da1
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-06-02 17:43:32 +0200

    feat: added a beatmaps attribute to osu cache

--- src/modules/osu/cache.ts
+++ src/modules/osu/cache.ts
@@ -1,20 +1,29 @@
 import { CommandInteraction } from 'discord.js';
 import { osuModule } from '.';
 import { extractCommandInfos } from '../../utils/commandInteraction';
+import { OsuBeatmap } from './api/types';
 
 export type OsuModuleCache = {
    associations: Record<string, string>;
+   beatmaps: Record<string, OsuBeatmap>;
 };
 
 export const defaultCache: OsuModuleCache = {
    associations: {},
+   beatmaps: {},
 };
 
 export const initializeCache = async (command: CommandInteraction) => {
    const { guildId } = extractCommandInfos(command);
+
    const cache = await osuModule.cache.get(guildId);
+   const cacheGlobal = await osuModule.cache.get('global');
 
    if (!cache) {
       osuModule.cache.set(guildId, defaultCache);
+   }
+
+   if (!cacheGlobal) {
+      osuModule.cache.set('global', defaultCache);
    }
 };
